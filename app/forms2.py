1198f98b119eab7b4bb921543bde3b9be5f43d11
Agata Martyna <agatamartynachalupka@gmail.com>
2021-01-24 00:14:14 +0100

    fix for PEP

--- app/forms2.py
+++ app/forms2.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
+from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
 class AuthorForm(FlaskForm):
     name = StringField("name", validators=[DataRequired()])
 
+
 class BookForm(FlaskForm):
     title = StringField("title", validators=[DataRequired()])
-
